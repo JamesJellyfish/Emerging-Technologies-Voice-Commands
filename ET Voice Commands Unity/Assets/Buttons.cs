772b17a33a43c2cbd64bbe0e2009fea0d5daa39a
James Pham <james.pham2@ontariotechu.net>
2023-04-03 04:53:59 -0400

    Unity Done

--- ET Voice Commands Unity/Assets/Buttons.cs
+++ ET Voice Commands Unity/Assets/Buttons.cs
@@ -1,16 +1,26 @@
 // https://www.youtube.com/watch?v=dzD0qP8viLw
+// https://www.youtube.com/watch?v=29vyEOgsW8s
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Windows.Speech;
+
+
+
 public class Buttons : MonoBehaviour
 {
+    public Transform transform;
     public GameObject Mute;
     public GameObject Unmute;
+    public GameObject VoiceDisable;
     public GameObject RaiseHand;
+    public GameObject help;
     bool Toggle = true;
     bool ToggleHand = true;
+    bool ToggleVoiceRec = true;
     public TextMeshProUGUI Language;
 
     public float moveSpeed;
@@ -28,27 +38,52 @@ public class Buttons : MonoBehaviour
     public float threshold = 0.1f;
     //
 
-    //private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
-    //private KeyWordRecognizer keywordRecognizer;
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, System.Action> actions = new Dictionary<string, System.Action>();
+
+    //
 
     // Start is called before the first frame update
     void Start()
     {
-        //keywordActions.Add("mute", MuteMic);
-        //keywordActions.Add("unmute", UnMute);
-        //keywordActions.Add("rise hand", RiseHand);
-        //keywordActions.Add("drop hand", DropHand);
-        //keywordActions.Add("english", English);
-        //keywordActions.Add("french", French);
-        //keywordActions.Add("vietnamese", Vietnamese);
+        //UI
+        actions.Add("mute", MuteMic);
+        actions.Add("unmute", UnMute);
+
+        actions.Add("hand up", RiseHand);
+        actions.Add("hand down", DropHand);
+
+        actions.Add("setting english", English);
+        actions.Add("setting french", French);
+        actions.Add("setting vietnamese", Vietnamese);
+
+        actions.Add("help", Help);
+        actions.Add("exit", Exit);
+
+        //Movements
+        actions.Add("move forward", Forward);
+        actions.Add("move back", Back);
+        actions.Add("move left", Left);
+        actions.Add("move right", Right);
+
+        //actions.Add("turn left", TurnLeft);
+        //actions.Add("turn right", TurnRight);
+
+        actions.Add("Desk 1", Desk1);
+        actions.Add("Desk 2", Desk2);
+        actions.Add("Desk 3", Desk3);
+
+
+        //
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
 
         English();
 
         Language.text = "English";
 
-        //keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
-        //keywordRecognizer.OnPhraseRecognized + OnKeyworldsRecognized;
-        //keywordRecognizer.Start();
+
     }
 
     // Update is called once per frame
@@ -88,31 +123,31 @@ public class Buttons : MonoBehaviour
             }
         }
 
+        //UI Language Setting
         if (Input.GetKeyDown(KeyCode.F))
             Language.text = "French";
         if (Input.GetKeyDown(KeyCode.E))
             Language.text = "English";
         if (Input.GetKeyDown(KeyCode.V))
             Language.text = "Vietnamese";
-        
-        //Audio
 
-        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+        //Start and Stop
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (ToggleVoiceRec)
+            {
+                keywordRecognizer.Stop();
+                VoiceDisable.SetActive(true);
 
-        if (loudness < threshold)
-            loudness = 0;
-        
-        if (loudness > 0)
-        {
-            RaiseHand.active = true;
-            Unmute.active = true;
-            Mute.active = false;
-        }
-        else
-        {
-            RaiseHand.active = false;
-            Mute.active = true;
-            Unmute.active = false;
+                ToggleVoiceRec = false;
+            }
+            else
+            {
+                keywordRecognizer.Start();
+                VoiceDisable.SetActive(false);
+
+                ToggleVoiceRec = true;
+            }
         }
     }
 
@@ -153,10 +188,65 @@ public class Buttons : MonoBehaviour
         Unmute.active = true;
     }
 
-    //private void OnKeyworldsRecognized(PhraseRecognizedEventArgs args)
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward()
+    {
+        transform.position = transform.position + new Vector3(0f, 0f, 2f);
+    }
+
+    private void Back()
+    {
+        transform.position = transform.position + new Vector3(0f, 0f, -2f);
+    }
+
+    private void Left()
+    {
+        transform.position = transform.position + new Vector3(-2f, 0f, 0f);
+    }
+
+    private void Right()
+    {
+        transform.position = transform.position + new Vector3(2f, 0f, 0f);
+    }
+
+    private void Help()
+    {
+        help.SetActive(true);
+    }
+
+    private void Exit()
+    {
+        help.SetActive(false);
+    }
+    
+    //private void TurnLeft()
     //{
-    //    Debug.Log("Keyword: " + args.text);
-    //    keywordActions[args.text].Invoke();
+    //    transform.rotation = new Vector3(0f, -45f, 0f);
     //}
+
+    //private void TurnRight()
+    //{
+    //    transform.rotation += new Vector3(0f, -45f, 0f);
+    //}
+
+    private void Desk1()
+    {
+        transform.position = new Vector3(0f, -0.05f, 5.79f);
+    }
+
+    private void Desk2()
+    {
+        transform.position = new Vector3(0f, -0.05f, -0.2f);
+    }
+
+    private void Desk3()
+    {
+        transform.position = new Vector3(0.04f, -0.05f, -5.28f);
+    }
 
 }
